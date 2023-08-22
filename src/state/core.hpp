fe1d032b3a436183c45bc48fb9315d74af07c533
JacquesRW <jamie.whiting2003@icloud.com>
2023-08-23 00:31:40 +0100

    core source file

--- src/state/core.hpp
+++ src/state/core.hpp
@@ -99,48 +99,9 @@ class LinkHead
         [[nodiscard]] constexpr auto len() const { return length; }
         [[nodiscard]] constexpr auto isEmpty() const { return length == 0; }
 
-        void join(LinkHead& other, std::vector<LinkNode>& tiles)
-        {
-            // overwrite group IDs
-            const auto newGroupId = first.isNull() ? 1024 : tiles[first.index()].group;
-            for (Tile tile = other.first; !tile.isNull(); tile = tiles[tile.index()].next)
-                tiles[tile.index()].group = newGroupId;
+        void join(LinkHead& other, std::vector<LinkNode>& tiles);
 
-            // join up the groups
-            if (!other.last.isNull())
-                tiles[other.last.index()].next = first;
-
-            if (!first.isNull())
-                tiles[first.index()].prev = other.last;
-
-            length += other.length;
-            if (!other.first.isNull())
-                first = other.first;
-            if (last.isNull())
-                last = other.last;
-
-            other = LinkHead{};
-        }
-
-        void remove(const Tile tile, std::vector<LinkNode>& tiles)
-        {
-            const auto index = tile.index();
-            const auto prev = tiles[index].prev;
-            const auto next = tiles[index].next;
-            tiles[index] = LinkNode{};
-
-            if (prev.isNull())
-                first = next;
-            else
-                tiles[prev.index()].next = next;
-
-            if (next.isNull())
-                last = prev;
-            else
-                tiles[next.index()].prev = prev;
-
-            length--;
-        }
+        void remove(const Tile tile, std::vector<LinkNode>& tiles);
 
     private:
         std::uint32_t length;
